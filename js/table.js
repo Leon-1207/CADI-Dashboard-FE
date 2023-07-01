bb1c24f1d1b48b4a0c6f634a38cee5a3ba9af7af
Leon Mainusch <leon.mainusch@web.de>
2023-07-01 15:01:35 +0200

    NEW: popup (WIP)

--- js/table.js
+++ js/table.js
@@ -1,9 +1,9 @@
 // JavaScript for sorting the table
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const table = document.querySelector("table");
   const headers = table.querySelectorAll("th");
-  headers.forEach(function(header) {
-    header.addEventListener("click", function() {
+  headers.forEach(function (header) {
+    header.addEventListener("click", function () {
       const sortOrder = this.dataset.sort === "asc" ? "desc" : "asc";
       const columnIndex = this.cellIndex;
 
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const tbody = table.querySelector("tbody");
     const rows = Array.from(tbody.querySelectorAll("tr"));
 
-    rows.sort(function(a, b) {
+    rows.sort(function (a, b) {
       const aValue = a.cells[columnIndex].textContent.trim();
       const bValue = b.cells[columnIndex].textContent.trim();
 
@@ -31,22 +31,22 @@ document.addEventListener("DOMContentLoaded", function() {
       tbody.removeChild(tbody.firstChild);
     }
 
-    rows.forEach(function(row) {
+    rows.forEach(function (row) {
       tbody.appendChild(row);
     });
   }
 
   // JavaScript for searching the table
   const searchInput = document.getElementById("search");
-  searchInput.addEventListener("input", function() {
+  searchInput.addEventListener("input", function () {
     const searchTerm = this.value.toLowerCase();
     const rows = table.querySelectorAll("tbody tr");
 
-    rows.forEach(function(row) {
+    rows.forEach(function (row) {
       const cells = row.querySelectorAll("td");
       let found = false;
 
-      cells.forEach(function(cell) {
+      cells.forEach(function (cell) {
         const cellValue = cell.textContent.toLowerCase();
 
         if (cellValue.includes(searchTerm)) {
@@ -61,4 +61,37 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     });
   });
+
+  // JavaScript for the popup
+  const popupOverlay = document.querySelector(".popup-overlay");
+  const popupContent = document.querySelector(".popup-content");
+
+  table.addEventListener("click", function (event) {
+    const target = event.target.closest("tr");
+
+    if (target) {
+      popupContent.innerHTML = target.innerHTML;
+      popupOverlay.style.display = "block";
+      popupContent.classList.add("open");
+
+      document.addEventListener("click", closePopup);
+    }
+  });
+
+  function closePopup(event) {
+    if (
+      !popupContent.contains(event.target) &&
+      !table.contains(event.target)
+    ) {
+      popupContent.classList.add("close");
+
+      setTimeout(function () {
+        popupOverlay.style.display = "none";
+        popupContent.classList.remove("open");
+        popupContent.classList.remove("close");
+      }, 300);
+
+      document.removeEventListener("click", closePopup);
+    }
+  }
 });
